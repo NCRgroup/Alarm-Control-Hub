c0d8216f777a97bf6655512553066fedd9897340
NCRgroup <161941477+NCRgroup@users.noreply.github.com>
2024-03-16 11:43:08 +0000

    Create login.js

---
+++ MainCode/login.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCnVJx2wX0x6VN339h1yTlxNrtLN5MAfRg",
+    authDomain: "ncr-alarm-hub-login.firebaseapp.com",
+    projectId: "ncr-alarm-hub-login",
+  storageBucket: "ncr-alarm-hub-login.appspot.com",
+  messagingSenderId: "517102115945",
+  appId: "1:517102115945:web:980c6a86a77b93f8fad6f8",
+  measurementId: "G-0YNBV3LZZL"
+};
+firebase.initializeApp(firebaseConfig);
